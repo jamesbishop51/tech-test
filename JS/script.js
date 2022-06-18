234c563ed9ade8ba1155893e5be403e55dbd7be3
James Bishop <bishbashbosh51@hotmail.com>
2022-06-18 15:01:02 +0100

    added centred box to canvas

--- JS/script.js
+++ JS/script.js
@@ -2,20 +2,34 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 var width = document.getElementById('width')
 var height = document.getElementById('height')
+var btn = document.getElementById('btn')
+
+windowheight = 168
+windowwidth = 154
 
 width.addEventListener("keyup", function(){
-    width=this.value/3;
-    draw();
+    width=this.value;    
 }, false);
 
 height.addEventListener("keyup", function(){
-    height=this.value/3;
-    draw();
+    height=this.value;   
 }, false);
-
+//min width => 750, max width => 900. min height => 1900, max height => 2100
+btn.addEventListener('click', function handleClick(event)  {
+    if((width >= 750 && width <= 900 )&&(height >= 1900 && height <= 2100)) {
+        
+        draw();
+    }else if(width < 750 || width >900){
+        alert("Please enter a width between 750 and 900mm");
+        
+    }
+    
+},false);
 
 function draw(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.fillStyle = 'green';
-    ctx.fillRect(40,40,width,height);
+    ctx.fillStyle = 'white';    
+    ctx.strokeRect(0,0,width,height);
+    ctx.fillStyle = "blue";
+    ctx.fillRect((width / 2) - (windowwidth / 2), (height / 2) - (windowheight / 2), windowwidth, windowheight);
 }
