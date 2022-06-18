fda4446286be287ff578b53fec7f5f6c5e3125bc
James Bishop <bishbashbosh51@hotmail.com>
2022-06-18 15:53:20 +0100

    tidied up code, added check for canvas

--- JS/script.js
+++ JS/script.js
@@ -7,33 +7,38 @@ var btn = document.getElementById('btn')
 windowheight = 168
 windowwidth = 154
 
-width.addEventListener("keyup", function(){
-    width=this.value;    
+width.addEventListener("keyup", function () {
+    width = this.value;
 }, false);
 
-height.addEventListener("keyup", function(){
-    height=this.value;   
+height.addEventListener("keyup", function () {
+    height = this.value;
 }, false);
 
-btn.addEventListener('click', function handleClick(event)  {
-    if(width < 750 || width >900){
-        alert("Please enter a width between 750 and 900mm");
-        document.getElementById('width').value='';
+btn.addEventListener('click', function handleClick(event) {
+    if (canvas.getContext) {
+        if (width < 750 || width > 900) {
+            alert("Please enter a width between 750 and 900mm");
+            document.getElementById('width').value = '';
+        }
+        else if (height < 1900 || height > 2100) {
+            alert("Please enter a heigth between 1900 and 2100mm");
+            document.getElementById('height').value = '';
+        }
+        else {
+            draw();
+        }
+    } else {
+        alert("this appliaction uses canvas which isnt supported by your cuirrent browers")
     }
-    else if(height < 1900 || height > 2100){
-        alert("Please enter a heigth between 1900 and 2100mm");
-        document.getElementById('height').value='';
-    }
-    else{
-        draw();
-    }
+
 
 })
 
-function draw(){
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.fillStyle = 'white';    
-    ctx.strokeRect(0,0,width,height);
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'white';
+    ctx.strokeRect(0, 0, width, height);
     ctx.fillStyle = "blue";
     ctx.fillRect((width / 2) - (windowwidth / 2), (height / 2) - (windowheight / 2), windowwidth, windowheight);
 }
