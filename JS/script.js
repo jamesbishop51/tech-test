9dfe308f8c92a84a1da2da8341bb36ee68633c81
James Bishop <bishbashbosh51@hotmail.com>
2022-06-22 15:51:35 +0100

    added colour selections, removed unnecessary code

--- JS/script.js
+++ JS/script.js
@@ -1,44 +1,51 @@
-const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-var width = document.getElementById('width')
-var height = document.getElementById('height')
-var btn = document.getElementById('btn')
 
-windowheight = 168
-windowwidth = 154
+var canvas = document.getElementById('canvas');
+var doorWidth = document.getElementById('doorWidth')
+var doorHeight = document.getElementById('doorHeight')
+var windowHeight = document.getElementById('windowHeight')
+var windowWidth = document.getElementById('windowWidth')
+var selectedColour = document.getElementById('colourSelection')
+var doorBtn = document.getElementById('btn')
 
-width.addEventListener("keyup", function () {
-    width = this.value;
+
+doorWidth.addEventListener("keyup", function () {
+    doorWidth = this.value;    
+}, false);
+doorHeight.addEventListener("keyup", function () {
+    doorHeight = this.value; 
+}, false);
+windowWidth.addEventListener("keyup", function () {
+    windowWidth = this.value;
+}, false);
+windowHeight.addEventListener("keyup", function () {
+    windowHeight = this.value;
 }, false);
 
-height.addEventListener("keyup", function () {
-    height = this.value;
-}, false);
 
-btn.addEventListener('click', function handleClick(event) {
-    //checks for canvas support 
-    if (canvas.getContext) {
+btn.addEventListener('click', function handleClick(event) {    
         //checks if inputs are between the correct values
-        if (width < 750 || width > 900) {
+        if (doorWidth < 750 || doorWidth > 900) {
             alert("Please enter a width between 750 and 900mm");
-            document.getElementById('width').value = '';
+            document.getElementById('doorWidth').value = '';
         }
-        else if (height < 1900 || height > 2100) {
+        else if (doorHeight < 1900 || doorHeight > 2100) {
             alert("Please enter a heigth between 1900 and 2100mm");
-            document.getElementById('height').value = '';
+            document.getElementById('doorHeight').value = '';
         }
         else {
             draw();
-        }
-    } else {
-        alert("this appliaction uses canvas which isnt supported by your cuirrent browers")
-    }
-})
+        }   
+    })
+
 function draw() {
     //draws the canvas
+    var option = selectedColour.options[selectedColour.selectedIndex];
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'white';
-    ctx.strokeRect(0, 0, width, height);
+    document.getElementById('canvas').width=doorWidth;
+    document.getElementById('canvas').height=doorHeight;
+    ctx.fillStyle = option.value;
+    ctx.fillRect(0, 0, doorWidth, doorHeight);
     ctx.fillStyle = "blue";
-    ctx.fillRect((width / 2) - (windowwidth / 2), (height / 2) - (windowheight / 2), windowwidth, windowheight);
+    ctx.fillRect((doorWidth / 2) - (windowWidth / 2), (doorHeight / 2) - (windowHeight / 2), windowWidth, windowHeight);
 }
